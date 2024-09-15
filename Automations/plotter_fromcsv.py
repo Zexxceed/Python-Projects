b8e3a44057499f3501adbc23bfb453e9dac13aec
Zexxceed <aries.seangoy@gmail.com>
2024-09-15 19:39:22 +0800

    Reworked plotter_fromcsv.py

--- Automations/plotter_fromcsv.py
+++ Automations/plotter_fromcsv.py
@@ -1,13 +1,11 @@
 import pandas as pd
 
-# Load the CSV file
-df = pd.read_csv('C:/Users/Zexxceed/Desktop/Python Projects/total_population_dupaxdelsur.csv')
+df = pd.read_csv('C:/Users/Zexxceed/Desktop/Work/Ecological Profile/CSV Files (EP)/total_population_dupaxdelsur.csv')
 
-# Example: Plot total population over time using Matplotlib
 import matplotlib.pyplot as plt
 
 plt.plot(df['Year'], df['Population'], marker='o')
-plt.title('Population Growth of Dupaxx Del Sur')
+plt.title('Population Growth of Dupax Del Sur')
 plt.xlabel('Year')
 plt.ylabel('Population')
 plt.show()
